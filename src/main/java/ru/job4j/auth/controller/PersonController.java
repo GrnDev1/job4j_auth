5bdbdc28e77e667d9db17c1c4b4b02b496c7a129
Roman <grndev@mail.ru>
2024-01-20 02:07:32 +0300

    Exception handling

--- src/main/java/ru/job4j/auth/controller/PersonController.java
+++ src/main/java/ru/job4j/auth/controller/PersonController.java
@@ -1,19 +1,28 @@
 package ru.job4j.auth.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import ru.job4j.auth.domain.Person;
 import ru.job4j.auth.service.PersonService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Collection;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/person")
 @AllArgsConstructor
+@Slf4j
 public class PersonController {
     private final PersonService persons;
+    private final ObjectMapper objectMapper;
 
     @GetMapping("/")
     public Collection<Person> findAll() {
@@ -24,18 +33,46 @@ public class PersonController {
     public ResponseEntity<Person> findById(@PathVariable int id) {
         return persons.findById(id)
                 .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.notFound().build());
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Person with this id is not found"
+                ));
     }
 
     @PostMapping("/")
     public ResponseEntity<Person> create(@RequestBody Person person) {
+        String login = person.getLogin();
+        String password = person.getPassword();
+        if (login == null || password == null) {
+            throw new NullPointerException("Login and password mustn't be empty");
+        }
+        if (password.length() < 5) {
+            throw new IllegalArgumentException("Invalid password. Password length must be more than 5 characters.");
+
+        }
         return persons.save(person)
                 .map(p -> new ResponseEntity<>(p, HttpStatus.CREATED))
                 .orElseGet(() -> new ResponseEntity<>(HttpStatus.CONFLICT));
     }
 
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    public void exceptionHandler(Exception e, HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setContentType("application/json");
+        response.getWriter().write(objectMapper.writeValueAsString(new HashMap<>() {
+            {
+                put("message", e.getMessage());
+                put("type", e.getClass());
+            }
+        }));
+        log.error(e.getLocalizedMessage());
+    }
+
     @PutMapping("/")
     public ResponseEntity<Void> update(@RequestBody Person person) {
+        String password = person.getPassword();
+        if (password == null) {
+            throw new NullPointerException("Password mustn't be empty");
+        }
         return persons.update(person) ? ResponseEntity.ok().build() : ResponseEntity.notFound().build();
     }
 
