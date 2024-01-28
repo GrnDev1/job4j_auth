46b9de11f01de1cf2efcaef62ffdf0799b1eaf2a
Roman <grndev@mail.ru>
2024-01-28 22:32:16 +0300

    Spring Liquibase

--- src/main/java/ru/job4j/auth/Job4jAuthApplication.java
+++ src/main/java/ru/job4j/auth/Job4jAuthApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Job4jAuthApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Job4jAuthApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Job4jAuthApplication.class, args);
+    }
 
 }
